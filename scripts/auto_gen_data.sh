6147d291bb20c3e950e4d7539fda2df779af3bcb
zhengcen <m@L1806-3>
2022-12-05 20:36:22 +0800

    fix some typos

--- scripts/auto_gen_data.sh
+++ scripts/auto_gen_data.sh
@@ -1,3 +1,4 @@
 python main.py -c configs/2pgcn/ntu_mutual/symmetry/xsub.yaml -gd
-
+python main.py -c configs/2pgcn/ntu_mutual/symmetry/xview.yaml -gd
 python main.py -c configs/2pgcn/ntu_mutual/symmetry/xsub120.yaml -gd 
+python main.py -c configs/2pgcn/ntu_mutual/symmetry/xset120.yaml -gd
